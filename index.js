067335e02fbc9c156e50a107bf7b776d7bfbbc23
AtulJoseph <AtulJoseph@users.noreply.github.com>
2023-03-05 09:44:31 +0530

    added two queries

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 const url = "mongodb+srv://Atul:12345abc@cluster0.ofsz1c6.mongodb.net/?retryWrites=true&w=majority"
   
 mongoose.connect(url).then((ans) => {
-  console.log("ConnectedSuccessful")
+  console.log("Connected successfully")
 }).catch((err) => {
   console.log("Error in the Connection")
 })
@@ -35,12 +35,14 @@ const collection_structure = new Schema({
 
 const collections = mongoose.model("tasks", collection_structure)
 
+//list all tasks
 app.get('/task', async (req, res) => {
     collections.find().then((ans) => {
         res.json(ans)
     })
 })
 
+//create new task
 app.post('/new', async (req, res) => {
     const tn = req.body.taskname
     const tid = req.body.taskid
@@ -52,11 +54,23 @@ app.post('/new', async (req, res) => {
     })
 })
 
+//complete a task
 app.post('/complete/:taskid', async (req, res) => {
     const tid = req.params.taskid
     collections.updateOne({taskid:tid}, {$set: {complete: true}}).then((ans) => res.json(ans))
 })
 
+//list all incomplete tasks
+app.get('/incomplete', async(req, res) => {
+    collections.find({complete: false}).then((ans) => res.json(ans))
+})
+
+//list all completed tasks
+app.get('/completed', async(req, res) => {
+    collections.find({complete: true}).then((ans) => res.json(ans))
+})
+
+//get progress of project
 app.get('/progress', async (req, res) => {
     const tnum = await collections.count().catch(console.error)
     const cnum = await collections.count({complete: true}).catch(console.error)
